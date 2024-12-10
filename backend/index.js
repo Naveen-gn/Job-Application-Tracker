e50e13762289b6062e33550a644916c29096b2bc
Naveen-gn <naveen2003gn@gmail.com>
2024-12-10 13:23:17 +0530

    signup, signin route finished

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log('MongoDB is connected'))
+  .catch(err => console.error(err));
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 app.use(cookieParser());
+
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
